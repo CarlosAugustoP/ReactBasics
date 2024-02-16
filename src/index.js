e4a8b861dc5382c04e581a50a5f6382c03b95926
Carlos Augusto <capv2004@gmail.com>
2024-02-15 23:13:11 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1 id = "title">
+        <span>supp guys!</span>
+        </h1>,
+    document.getElementById('root')
+);
